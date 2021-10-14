e605baf1f89f18e5745829dc92a59911bbbe4e09
Steve Brownlee <chortlehoort@gmail.com>
2021-10-14 10:37:47 -0500

    First event listener

--- scripts/Artists.js
+++ scripts/Artists.js
@@ -1,20 +1,50 @@
 import { getArtists } from "./database.js"
 
+// Invoke function to get copy of artist object array
 const artistArray = getArtists()
 
+document.addEventListener(
+  "click",
+  (theActualClickEvent) => {
+    // Get the element the user clicked
+    const elementClickedOn = theActualClickEvent.target
+
+    if (elementClickedOn.id.startsWith("the--artist--")) {
+      // Get the primary key from the id attribute string
+      const [, , artistId] = elementClickedOn.id.split('--')   // "2"
+
+      // Go get the actual object from the database that has that primary key
+      for (const artist of artistArray) {
+        if (artist.id === parseInt(artistId)) {
+          // Get the website property from that object
+          // Display website in an alert
+          window.alert(`The artist's website is ${artist.website}`)
+        }
+      }
+    }
+  }
+)
+
+const buildHTMLListItem = (artist) => {
+  return `
+    <li id="the--artist--${artist.id}">
+      ${artist.name}
+    </li>
+  `
+}
+
+/*
+  RESPONSIBILITY:
+  Produce HTML representation of artist objects
+*/
 export const Artists = () => {
   let artistHTML = "<ol>"
 
   for (const artistObj of artistArray) {
-    artistHTML += `
-      <li>
-        ${artistObj.name}
-      </li>
-    `
+    artistHTML += buildHTMLListItem(artistObj)
   }
 
   artistHTML += "</ol>"
 
   return artistHTML
-
 }
--- scripts/main.js
+++ scripts/main.js
@@ -2,4 +2,5 @@ import { Artists } from "./Artists.js"
 
 const mainContainer = document.querySelector("#sallysShows")
 
+// Invoke function to generate HTML for artists and inject into the DOM
 mainContainer.innerHTML = Artists()
